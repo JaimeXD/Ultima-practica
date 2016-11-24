a45fa45912332ac3abb99b9781f8c937df7b08e3
JaimeXD <jaime.obando@ucsp.edu.pe>
2016-11-24 09:19:42 -0500

    Add files via upload

---
+++ Graph/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+class Graph{
+private:
+    map<int,vector<int> > nodo;
+public:
+    Graph( vector<int>&starts, vector<int> &ends){
+        int i;
+        for(i = 0 ; i < starts.size() ;i++)
+            graf[starts[i]].push_back(fin[i]);
+    }
+    int numOutgoing(const int nodeID) const{
+        return adjacent(nodoID) ;
+    }
+    const vector<int> adjacent(const int nodeID) const{
+        return graf.find(ID_nodo) -> second;
+    }
+};
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
